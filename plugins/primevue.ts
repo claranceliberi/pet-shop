fd0d6f9e71e2263eb957b122aa3a02d16acf78fe
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-08-25 01:23:56 +0200

    feat: tell nuxt to ignore this as plugin

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,5 +1,5 @@
 import { defineNuxtPlugin } from "#app"
-import { registerPrimeVue } from "./registerPrimeVue"
+import { registerPrimeVue } from "./primeVue/registerPrimeVue"
 
 export default defineNuxtPlugin((nuxtApp) => {
     registerPrimeVue(nuxtApp.vueApp)
