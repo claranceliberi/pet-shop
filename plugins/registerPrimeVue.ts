dea68aa683f5f7b72ef934bbe57566c1268ac867
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-08-25 01:18:52 +0200

    refactor: removing unstyled code

--- plugins/registerPrimeVue.ts
+++ plugins/registerPrimeVue.ts
@@ -2,24 +2,10 @@ import PrimeVue from "primevue/config"
 import Button from "primevue/button"
 import InputText from "primevue/inputtext"
 
-import TailwindPassThrough from "primevue/passthrough/tailwind"
 import { App } from "vue"
 
-type TailwindType = typeof TailwindPassThrough
-
 export function registerPrimeVue(app: App) {
-    const Tailwind = removeUnwantedStyles(["button"])
-
     app.use(PrimeVue)
     app.component("PButton", Button)
     app.component("PInputText", InputText)
-}
-
-// remove styles which are colliding with my custom styles
-function removeUnwantedStyles(elements: (keyof TailwindType)[]) {
-    elements.forEach((element) => {
-        delete TailwindPassThrough[element]
-    })
-
-    return TailwindPassThrough
 }
