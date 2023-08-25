90f1dd4313a4c63bd2a02caf058daa14cf5f8d63
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-08-25 23:24:44 +0200

    test: button test

--- components/atoms/button/SButton.cy.ts
+++ components/atoms/button/SButton.cy.ts
@@ -1,8 +1,24 @@
 import SButton from "./SButton.vue"
 
 describe("<SButton />", () => {
-    it("renders", () => {
+    it("should have label", () => {
         // see: https://on.cypress.io/mounting-vue
-        cy.mount(SButton)
+        cy.mount(SButton, {
+            props: {
+                label: "Test Button",
+            },
+        })
+
+        cy.get("button").should("be.visible").and("contain", "Test Button")
+    })
+
+    it('should display Icon when "icon" prop is passed', () => {
+        cy.mount(SButton, {
+            props: {
+                icon: "pi pi-check",
+            },
+        })
+
+        cy.get("span.pi-check.p-button-icon").should("be.visible")
     })
 })
